67f27b3db95616b20d50d5e72d37fddd3768e54a
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2024-01-21 12:38:03 +0100

    Added more test cases

---
+++ 0x02-minimum_operations/main_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Main file for copy/paste algorithm
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+print(minOperations(19170307))  # Expecting 47613
+print(minOperations(972))   # Expecting 19
+print(minOperations(2147483640))    # Expecting 326
