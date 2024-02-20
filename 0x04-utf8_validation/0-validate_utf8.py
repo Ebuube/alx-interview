7897e1564f93d4bf7e675e5cfa19ee14f7b46699
Ebuube <onwutaebubegideon1555@gmail.com>
2024-02-20 16:46:56 +0000

    Removed tests and added more comments

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,15 +15,20 @@ def validUTF8(data):
     The data will be represented by a list of integers
     Each integer represents 1 byte of data, therefore you only need to
     handle the 8 least significant bits of each integer
+
+    - CONCEPT -
+    For a single-byte Character:
+        * The byte starst with a `0`
+        * It represents a single character in Unicode character set
     """
     if not isinstance(data, list):
         return False
 
     for num in data:
-        print("Testing: {}".format(num))    # test
         if not isinstance(num, int):
             return False
 
+        # Get only the least significant byte
         one_byte = num & 0xFF
 
         if not ((one_byte >> 7) ^ 0b1):
