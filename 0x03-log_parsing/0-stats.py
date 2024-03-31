5193943a61bc330be6bde6844802dac17e0f7dba
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-31 07:57:56 +0000

    Can calculate total file size

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,22 +4,30 @@
 import re
 import sys
 
-old_pattern = r'\A(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)\Z'
-
-new_pattern = r'(\w+) (\w+) "GET /projects/260 HTTP/1\.1 (\d+) (\d+)'
-
-prev_pattern = r'^<(\d+\.\d+\.\d+\.\d+)> - \[(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\] "GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)$'
-
-next_pattern = r'(\S+)\s+-\s+\[(\d+\-\d+\-\d+\s*\d+:\d+:\d+\.\d+)\] [^"]*" (\d{3}) (\d+)'
 
 pattern = '^(\S+) - \[(\S+) (\S+)\] "GET \/projects\/260 HTTP\/1\.1" (\d+)\s(\d+)$'
-for line in sys.stdin:
-    if line:
-        print("Line:", line)    # test
-        match = re.match(pattern, line)
-        print("Match:", match)  # test
-        if not match:
-            continue
-        for i in range(1, 6):
-            print(match.group(i), end='')
-        print()
+def analyze(line: str = '', payload: dict = {}):
+    """Analyze lines of input
+    """
+    if not line or type(line) is not str:
+        return
+
+    match = re.match(pattern, line)
+    if not match:
+        return
+    payload['file_size'] += int(match.group(5))
+    print(f"Line: {match.group(1)}\tFile size: {match.group(5)}") # test
+
+
+if __name__ == '__main__':
+    total_file_size = 0
+    payload = {'file_size': 0}
+    count = 0
+    for line in sys.stdin:
+        if line:
+            analyze(line, payload)
+        if count == 9:
+            print("Total file size:", payload['file_size'])
+            count = 0
+        else:
+            count += 1
