521da29ee1e10e8418fc18db73734d2189413830
Ebuube <onwutaebubegideon1555@gmail.com>
2024-04-26 09:58:37 +0000

    Pycodestyle compliant

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -18,7 +18,8 @@ def rotate_2d_matrix(matrix):
 
     for row in range(length):
         for col in range(start, length):
-            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+            (matrix[row][col], matrix[col][row]) = (matrix[col][row],
+                                                    matrix[row][col])
         start += 1
 
     for row in range(length):
